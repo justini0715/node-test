3ab77f85249210bdedb32bf9aa2a46895a6c4993
changwpa <changwpa@home-vm>
2026-01-21 03:54:17 +0900

    feat(index.ts): querystring check deleted. pathname check with split, not /.

--- src/index1.ts
+++ src/index1.ts
@@ -6,15 +6,11 @@ const server = http.createServer((req, res) => {
   const url = req.url ?? '/';
   const method = req.method ?? 'GET';
 
-  // 🔧 쿼리스트링 제거
-  const pathname = url.split('?')[0];
-
   console.log('--- REQUEST ---');
   console.log('method:', method);
   console.log('url:', url);
-  console.log('pathname:', pathname);
 
-  if (method === 'GET' && pathname === '/') { // 🔧 url → pathname
+  if (method === 'GET' && url === '/') {
     const filePath = path.join(__dirname, '../public/index.html');
     const html = fs.readFileSync(filePath);
     console.log('__dirname:', __dirname);
@@ -26,7 +22,7 @@ const server = http.createServer((req, res) => {
     return;
   }
 
-  if (method === 'GET' && pathname === '/ping') { // 🔧 url → pathname
+  if (method === 'GET' && url === '/ping') {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain; charset=utf-8');
     res.end('pong');
