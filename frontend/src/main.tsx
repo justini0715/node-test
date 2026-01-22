a82c8b8061006e1677ab01b2f94d65ffdf5acfaf
changwpa <changwpa@home-vm>
2026-01-22 10:13:34 +0900

    feat(vite-react-deprecated): depreacate vite-react, jump to Next.js + Exress.js

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDom from 'react-dom/client'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+import App from './App.tsx'
+import './index.css'
+
+ReactDom.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
